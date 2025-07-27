d4e30f65d53a21f8b1ba1d7af84d92afce40e30e
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-27 09:25:48 +0900

    init commit

---
+++ src/main/java/com/example/devcontainer/DevcontainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.devcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcontainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devcontainer/DevcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
